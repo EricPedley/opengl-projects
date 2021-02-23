1ed0921e9b7c89c4c68124eef460b76f39983d0d
Eric Pedley <ericpedley@gmail.com>
2021-02-22 20:43:20 -0800

    put two triangles on screen and used vertex array object

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import glfw
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
+from os import read
 
 vertex_src = """
 # version 330
@@ -43,42 +44,66 @@ glfw.set_window_pos(window, 400, 200)
 # make the context current
 glfw.make_context_current(window)
 
-vertices = [-0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-             0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+vertices = [0.0, -0.5, 0.0, 1.0, 0.0, 0.0,
+             0.5, 0.0, 0.0, 0.0, 1.0, 0.0,
+             0.0,  0.5, 0.0, 0.0, 0.0, 1.0,
+             -0.5, 0.0, 0.0, 1.0, 0.5, 0.0,
+             0.0, -0.5, 0.0, 0.0, 1.0, 0.0,
              0.0,  0.5, 0.0, 0.0, 0.0, 1.0]
 
 vertices = np.array(vertices, dtype=np.float32)
 
+
+# vertexShader = glCreateShader(GL_VERTEX_SHADER)
+# vertexShader = glShaderSource(vertexShader,1,vertex_src)
+# compileShader(vertex_src)
 shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER), compileShader(fragment_src, GL_FRAGMENT_SHADER))
 
-VBO = glGenBuffers(1)
-glBindBuffer(GL_ARRAY_BUFFER, VBO)
-glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+VAO = glGenVertexArrays(1)
+glBindVertexArray(VAO)
 
-position = glGetAttribLocation(shader, "a_position")
+VBO = glGenBuffers(1)#VBO stands for "vertex buffer object", and this is just the integer ID of that object
+glBindBuffer(GL_ARRAY_BUFFER, VBO)#binds the GL_ARRAY_BUFFER to the buffer we just made, so now referencing GL_ARRAY_BUFFER means referencing the vbo object we made
+glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)#puts the vertex data into the buffer we created
+
+indices = [0,1,2,
+        4,5,6]
+indices = np.array(indices,dtype=np.uint32)
+
+EBO = glGenBuffers(1)
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO)
+glBufferData(GL_ELEMENT_ARRAY_BUFFER,indices.nbytes,indices,GL_STATIC_DRAW)
+
+position = glGetAttribLocation(shader, "a_position")#gets the integer position of the a_position variable from the shader, which we could've set with "layout (location=0) in vec3 a_position"
 glEnableVertexAttribArray(position)
-glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))#starts at 0 and stride is 24 bytes, so it skips over the color info that follows each vertex
 
-color = glGetAttribLocation(shader, "a_color")
+color = glGetAttribLocation(shader, "a_color")#integer location of a_color variable in the shader.
 glEnableVertexAttribArray(color)
-glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+glVertexAttribPointer(color, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))#starts at 12 abd strude us 24, so it starts at the color and skips the vertex info
 
 glUseProgram(shader)
-glClearColor(0, 0.1, 0.1, 1)
+glClearColor(0, 0.1, 0.1, 1)#sets clear color
 
 def framebuffer_size_callback(window,width,height):
     glViewport(0,0,width,height)
+
+def process_input(window):
+    if glfw.get_key(window,glfw.KEY_ESCAPE) == glfw.PRESS:
+        glfw.set_window_should_close(window,True)
 
 glfw.set_framebuffer_size_callback(window,framebuffer_size_callback)
 
 # the main application loop
 while not glfw.window_should_close(window):
-    glfw.poll_events()
-
+    process_input(window)
     glClear(GL_COLOR_BUFFER_BIT)
 
-    glDrawArrays(GL_TRIANGLES, 0, 3)
+    #glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO)
+    glDrawArrays(GL_TRIANGLES, 0,6)
+    #glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0)#gets stuck on this line
 
+    glfw.poll_events()
     glfw.swap_buffers(window)
 
 # terminate glfw, free up allocated resources
