264a5e642dc996c754f0c2e6464a47429e070172
Eric Pedley <ericpedley@gmail.com>
2021-02-26 15:09:34 -0800

    put prediction code in a class

--- imgtomesh/depth_prediction/predict.py
+++ imgtomesh/depth_prediction/predict.py
@@ -10,19 +10,38 @@ from PIL import Image
 import fcrn as models
 tf.disable_v2_behavior()
 
-def simplePredict(img,session,net,input_node):
-    # Default input size
-    height = 228
-    width = 304
-    channels = 3
-    batch_size = 1
-    img = img.resize([width,height], Image.ANTIALIAS)
-    img = np.array(img).astype('float32')
-    img = np.expand_dims(np.asarray(img), axis = 0)
-    pred = session.run(net.get_output(), feed_dict={input_node: img})
-    return pred[0,:,:,0]
-    
+class DepthMapGenerator:
+    def __init__(self):
+        # Default input size
+        height = 228
+        width = 304
+        #height and width for inputs into the network
+        self.inputHeight=height
+        self.inputWidth=width
+        channels = 3
+        batch_size = 1
+        # Create a placeholder for the input image
+        self.input_node = tf.placeholder(tf.float32, shape=(None, height, width, channels))
+        self.net = models.ResNet50UpProj({'data': input_node}, batch_size, 1, False)
+        self.sess=tf.Session()#https://www.tensorflow.org/api_docs/python/tf/compat/v1/Session
+        # Use to load from ckpt file
+        saver = tf.train.Saver()     
+        saver.restore(sess, "NYU_FCRN-checkpoint/NYU_FCRN.ckpt")
 
+        # Use to load from npy file
+        #net.load(model_data_path, sess)
+        
+    def __del__(self):
+        self.sess.close()
+
+    #img should be PIL image object
+    def getPrediction(self,img):
+        # Evalute the network for the given image
+        img = img.resize([self.inputWidth,self.inputHeight], Image.ANTIALIAS)
+        img = np.array(img).astype('float32')
+        img = np.expand_dims(np.asarray(img), axis = 0)
+        pred = self.sess.run(self.net.get_output(), feed_dict={self.input_node: self.img})
+        return pred[0,:,:,0]
 
 def predict(model_data_path, image_path,sess=None,net=None,input_node=None):
 
